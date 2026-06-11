2927c85f71bbf9e6a2642ce0e89cb8f06f6435df
sojibulislamrana <sojibulislamrana.dev@gmail.com>
2026-06-11 12:34:40 +0600

    feature: router setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { client } from "@/lib/client";
 import { useMutation } from "@tanstack/react-query";
 import { nanoid } from "nanoid";
+import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const ANIMALS = ["wolf", "eagle", "bear", "panda", "tiger"];
@@ -15,6 +16,7 @@ const generateUsername = () => {
 
 export default function Home() {
   const [username, setUsername] = useState("RANA_ASIF");
+  const router = useRouter();
 
   useEffect(() => {
     const main = () => {
@@ -34,6 +36,10 @@ export default function Home() {
   const { mutate: createRoom } = useMutation({
     mutationFn: async () => {
       const res = await client.room.create.post();
+
+      if (res.status == 200) {
+        router.push(`/rooms/${res.data?.roomId}`);
+      }
     },
   });
 
