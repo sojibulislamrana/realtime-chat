85ababde5ba341b018dcfba7e6a0868168d23156
sojibulislamrana <sojibulislamrana.dev@gmail.com>
2026-06-11 12:54:32 +0600

    feature: dynamic roomId routes

--- src/app/api/[[...slugs]]/route.ts
+++ src/app/api/[[...slugs]]/route.ts
@@ -1,20 +1,20 @@
 import { redis } from "@/lib/redis";
-import { Elysia }from "elysia";
+import { Elysia } from "elysia";
 import { nanoid } from "nanoid";
 
-const ROOM_TTL_SECONDS  = 60 * 10 ;
+const ROOM_TTL_SECONDS = 60 * 10;
 
-const rooms = new Elysia({ prefix: "/room"}).post("/create", async () => {
-    const roomId = nanoid();
-    await redis.hset(`meta:${roomId}`, {connected: [], createdAt: Date.now(),});
-    await redis.expire(`meta:${roomId}`,  ROOM_TTL_SECONDS);
+const rooms = new Elysia({ prefix: "/room" }).post("/create", async () => {
+  const roomId = nanoid();
+  await redis.hset(`meta:${roomId}`, { connected: [], createdAt: Date.now() });
+  await redis.expire(`meta:${roomId}`, ROOM_TTL_SECONDS);
 
-    return {roomId};
-})
+  return { roomId };
+});
 
-const app = new Elysia({ prefix: "/api"}).use(rooms);
+const app = new Elysia({ prefix: "/api" }).use(rooms);
 
-export const GET = app.fetch
-export const POST = app.fetch
+export const GET = app.fetch;
+export const POST = app.fetch;
 
 export type App = typeof app;
